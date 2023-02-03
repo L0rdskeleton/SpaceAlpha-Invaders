d2e4633b60b6b70e8b8bdadd62e0b61859fbb8ac
L0rdskeleton <124097102+L0rdskeleton@users.noreply.github.com>
2023-02-03 23:17:42 +0000

    shortening

--- index.js
+++ index.js
@@ -415,12 +415,7 @@ function animate() {
     );
   }
   function randomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+  '#' += Math.floor(Math.random()*16777215).toString(16);
 }
   if((keys.space.pressed && !game.over) || (keys.mouse.pressed && !game.over)){
     audio.shoot.play();

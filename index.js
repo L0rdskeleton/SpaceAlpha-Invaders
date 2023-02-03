1192855ee36ddbeb28fb6fcbe57b1e60cdd334bb
L0rdskeleton <124097102+L0rdskeleton@users.noreply.github.com>
2023-02-03 18:45:25 -0500

    revert

--- index.js
+++ index.js
@@ -414,8 +414,14 @@ function animate() {
       Projectile.createProjectile(0, -10, "yellow")
     );
   }
-  var randomColor = "#" += Math.floor(Math.random()*16777215).toString(16);
-  
+  function randomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
   if((keys.space.pressed && !game.over) || (keys.mouse.pressed && !game.over)){
     audio.shoot.play();
     projectiles.push(

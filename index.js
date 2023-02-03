834d5db64f69ecbcd69eff38cbc33078eb2d13da
L0rdskeleton <124097102+L0rdskeleton@users.noreply.github.com>
2023-02-03 18:29:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -415,7 +415,7 @@ function animate() {
     );
   }
   function randomColor() {
-  '#' += Math.floor(Math.random()*16777215).toString(16);
+  "#" += Math.floor(Math.random()*16777215).toString(16);
 }
   if((keys.space.pressed && !game.over) || (keys.mouse.pressed && !game.over)){
     audio.shoot.play();
